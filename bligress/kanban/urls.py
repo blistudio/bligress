4ff9d6f04315ad04744b3bb01ac73d94ac334517
Jouni Roivas <jroivas@iki.fi>
2013-07-30 21:38:52 +0300

    Move tasks

--- bligress/kanban/urls.py
+++ bligress/kanban/urls.py
@@ -4,6 +4,8 @@ from kanban import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^task/(?P<task_id>\d+)/$', views.showtask, name='showtask'),
-    url(r'^board/(?P<board_id>\d+)/$', views.showboard, name='showboard')
+    url(r'^task/(?P<task_id>\d+)/$', views.taskshow, name='taskshow'),
+    url(r'^taskback/(?P<board_id>\d+)/(?P<task_id>\d+)/$', views.taskback, name='taskback'),
+    url(r'^taskforw/(?P<board_id>\d+)/(?P<task_id>\d+)/$', views.taskforw, name='taskforw'),
+    url(r'^board/(?P<board_id>\d+)/$', views.boardshow, name='boardshow')
 )
--- bligress/kanban/views.py
+++ bligress/kanban/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.http import Http404
 
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 from kanban.models import Task
 from kanban.models import Board
@@ -9,7 +10,7 @@ from kanban.models import Board
 def index(request):
     return HttpResponse("Heippa.")
 
-def showtask(request, task_id):
+def taskshow(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except:
@@ -17,7 +18,56 @@ def showtask(request, task_id):
 
     return render(request, 'task.html', {'task': task})
 
-def showboard(request, board_id):
+def taskforw(request, board_id, task_id):
+    try:
+        board = Board.objects.get(pk=board_id)
+    except:
+        raise Http404
+    try:
+        task = Task.objects.get(pk=task_id)
+    except:
+        raise Http404
+
+    nextphase = None
+    getnext = False
+    for phase in board.phases.order_by('order'):
+        if getnext:
+            nextphase = phase
+            break 
+        if phase == task.phase:
+            getnext = True
+
+    if nextphase is not None:
+        task.phase = nextphase
+        task.save()
+
+    return redirect('/kanban/board/%s' % (board_id))
+
+def taskback(request, board_id, task_id):
+    try:
+        board = Board.objects.get(pk=board_id)
+    except:
+        raise Http404
+    try:
+        task = Task.objects.get(pk=task_id)
+    except:
+        raise Http404
+
+    prevphase = None
+    getprev = False
+    for phase in board.phases.order_by('order'):
+        if phase == task.phase:
+            getprev = True
+            break
+        prevphase = phase
+
+    if getprev and prevphase is not None:
+        task.phase = prevphase
+        task.save()
+
+    return redirect('/kanban/board/%s' % (board_id))
+
+def boardshow(request, board_id):
     try:
         board = Board.objects.get(pk=board_id)
     except:
