16a563425722fc32ed1b481b49e829a4aea4d43f
Jouni Roivas <jouni.roivas@blistud.io>
2013-10-25 16:42:55 +0000

    Add filters

---
+++ kanban/templatetags/taskboard.py
@@ -0,0 +1,20 @@
+from django import template
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
+
+register = template.Library()
+
+import re
+
+@register.filter()
+def itemsurl(value, board_id):
+    matches = re.findall('TASK-(\d+)', value)
+    for match in matches:
+        location = reverse('kanban:taskshow', args=[board_id, match]);
+        value = value.replace('TASK-%s' % (match), '<a href="%s">TASK-%s</a>' % (location, match))
+    matches = re.findall('BOARD-(\d+)', value)
+    for match in matches:
+        location = reverse('kanban:boardshow', args=[board_id]);
+        value = value.replace('BOARD-%s' % (match), '<a href="%s">BOARD-%s</a>' % (location, match))
+    return mark_safe(value)
+
